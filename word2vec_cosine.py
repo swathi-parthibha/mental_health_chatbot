3862d64f41ff6fd3813bfdd99f7beae08bff28e1
Ria Vora <rhv26@cornell.edu>
2023-04-07 16:19:47 -0400

    Fixing word2Vec and stemmer

--- word2vec_cosine.py
+++ word2vec_cosine.py
@@ -1,6 +1,8 @@
 import json
 import numpy as np
 from gensim.models import Word2Vec
+from nltk.stem.snowball import SnowballStemmer
+
 
 # get the user input 
 user_input = input("Input: ")
@@ -20,15 +22,34 @@ cleaned_patterns = ["".join(
     [char for char in item if char not in punctuation_to_remove]) for item in cleaned_patterns]
 
 # add the input
-# clean the input by only keeping the words that are used as feature names
+# cleans the input by removing punctuation
 user_input_words = [item.lower() for item in user_input.split(" ")]
 user_input_cleaned = ["".join(
     [char for char in item if char not in punctuation_to_remove]) for item in user_input_words]
 
+#implementing the snowball stemmer on the input
+stemmer = SnowballStemmer("english")
+user_input_stemmed = []
+for word in user_input_cleaned: 
+	user_input_stemmed += [stemmer.stem(word)]
+
+print(user_input_stemmed)
+
+# further cleans the input by removing words that are not in the pattern
+# we need to do this because the model created is using the words in the cleaned_patterns
+pattern_words = []
+for sentence in cleaned_patterns: 
+	pattern_words += sentence.split()
+
+for word in user_input_stemmed: 
+	if word not in pattern_words: 
+		user_input_stemmed.remove(word)
+
+print(user_input_stemmed)
+
 # trying the word2vec vectorizer
 sentences = [sentence.split(" ") for sentence in cleaned_patterns]
 model = Word2Vec(sentences=sentences, vector_size=100, window=5, min_count=1, workers=4)
-
 
 # find the largest cosine similarity and index where it was found
 max_cosine = 0
@@ -40,7 +61,7 @@ for pattern in cleaned_patterns:
 	v1 = np.array([model.wv[word] for word in words])
 
 	# input words
-	v2 = np.array([model.wv[word] for word in user_input_cleaned])
+	v2 = np.array([model.wv[word] for word in user_input_stemmed])
 
 	l1 = np.mean(v1, axis=0)
 	l2 = np.mean(v2, axis=0)
