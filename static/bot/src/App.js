a9231c6cb7fc2ca6c6b313fa80c6c41d58d8102d
Ria Vora <rhv26@cornell.edu>
2023-04-12 17:15:09 -0400

    More React App Setup

--- static/bot/src/App.js
+++ static/bot/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+<head>
+  <link rel="stylesheet" href="{{ url_for('static', filename='output.css') }}"/>
+  <link href="https://cdn.jsdelivr.net/npm/daisyui@2.51.5/dist/full.css" rel="stylesheet" type="text/css" />
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+
+<div class="chat chat-start">
+  <div class="chat-image avatar">
+    <div class="w-10 rounded-full">
+      <img src="/static/images/bot.jpeg" />
     </div>
+  </div>
+  <div class="chat-bubble chat-bubble-secondary">{"hi"}</div>
+</div>
+
+<div class="chat chat-end">
+  <div class="chat-image avatar">
+    <div class="w-10 rounded-full">
+      <img src="/static/images/person.png" />
+    </div>
+  </div>
+  <div class="chat-bubble chat-bubble-primary">{"hi"}</div>
+</div>
+
+<form method="POST">
+  <input type="text" placeholder="Type here" class="input input-bordered input-sm w-full max-w-xs" id="user_input" name="user_input"/>
+</form>
+
+</body>
+   </div>
   );
 }
 
