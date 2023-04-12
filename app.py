a1d747f44ac579dce934a7b5a14e8f735e1dc2a9
Ria Vora <rhv26@cornell.edu>
2023-04-12 18:14:45 -0400

    More react setup

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ from bert import execute
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST'])
-def hello():
-    if request.method == 'POST': 
-        user_input = request.form.get("user_input")
-        output = execute(user_input)
-        return render_template('index.html', user_input = user_input, bot_output = output)
-    else: 
-        return render_template('index.html')
+# @app.route('/', methods = ['GET', 'POST'])
+# def hello():
+#     if request.method == 'POST': 
+#         # user_input = request.form.get("user_input")
+#         # output = execute(user_input)
+#         # return render_template('index.html', user_input = user_input, bot_output = output)
+#         return {"hi": "hello"}
+#     else: 
+#         return {"hi2": "hello2"}
+#         # return render_template('index.html')
+
+@app.route('/test', methods = ['GET', 'POST'])
+def test(): 
+    return {"hi2": "hello2"}
 
 
--- static/bot/src/App.js
+++ static/bot/src/App.js
@@ -1,40 +1,23 @@
 import React, {useState, useEffect} from 'react'
 
 function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch('/test')
+    .then(res => res.json())
+    .then(data => {
+      setData(data)
+      console.log(data)
+    })
+  }, [])
+
   return (
    <div>
-<head>
-  <link rel="stylesheet" href="{{ url_for('static', filename='output.css') }}"/>
-  <link href="https://cdn.jsdelivr.net/npm/daisyui@2.51.5/dist/full.css" rel="stylesheet" type="text/css" />
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
-
-<div class="chat chat-start">
-  <div class="chat-image avatar">
-    <div class="w-10 rounded-full">
-      <img src="/static/images/bot.jpeg" />
-    </div>
-  </div>
-  <div class="chat-bubble chat-bubble-secondary">{"hi"}</div>
-</div>
-
-<div class="chat chat-end">
-  <div class="chat-image avatar">
-    <div class="w-10 rounded-full">
-      <img src="/static/images/person.png" />
-    </div>
-  </div>
-  <div class="chat-bubble chat-bubble-primary">{"hi"}</div>
-</div>
-
-<form method="POST">
-  <input type="text" placeholder="Type here" class="input input-bordered input-sm w-full max-w-xs" id="user_input" name="user_input"/>
-</form>
-
-</body>
+     
    </div>
-  );
+  )
 }
 
 export default App;
