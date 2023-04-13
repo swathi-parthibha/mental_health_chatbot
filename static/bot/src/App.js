a2b134c5a3d34b11c39c85126f62160a782e7ead
Ria Vora <rhv26@cornell.edu>
2023-04-13 18:45:12 -0400

    Setting up form in React

--- static/bot/src/App.js
+++ static/bot/src/App.js
@@ -4,19 +4,48 @@ function App() {
 
   const [data, setData] = useState([])
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   fetch('/test')
+  //   .then(res => res.text())
+  //   .then(data => {
+  //     setData(previous_data => [...previous_data, data])
+  //     console.log(data)
+  //   })
+  // }, [])
+
+  const handleClick = (event) => {
     fetch('/test')
     .then(res => res.text())
     .then(data => {
       setData(previous_data => [...previous_data, data])
       console.log(data)
     })
-  }, [])
+    event.preventDefault()
+    console.log("out here")
+    return false;
+  }
+
+  const handleClickButton = () => {
+    fetch('/test')
+    .then(res => res.text())
+    .then(data => {
+      setData(previous_data => [...previous_data, data])
+      console.log(data)
+    })
+    return false;
+  }
+
+  const handleSubmit = () => {
+    console.log("submitted")
+  }
 
   return (
    <div>
+      <button onClick={handleClickButton}>Fetch data</button>
+      <form onSubmit={event => handleClick(event)}>
+        <input />
+      </form>
      {data && data.map((member, i) => <p key = {i}> {member} </p>)}
-
    </div>
   )
 }
